c659e2620223df1f3432ae7a08295fd07c14028d
Alex H <48185123+Vodes@users.noreply.github.com>
2024-03-16 22:40:02 +0100

    Bump version & dependencies, add slf4j logging & config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.serialization") version "1.9.21"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "pw.vodes"
-version = "1.0.1"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -15,28 +15,19 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("net.peanuuutz.tomlkt:tomlkt:0.3.7")
     implementation("org.javacord:javacord:3.8.0")
+    implementation("org.slf4j:slf4j-simple:2.0.12")
+    runtimeOnly("org.apache.logging.log4j:log4j-to-slf4j:2.23.1")
 
     // https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit
-    implementation("org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.9.0.202403050737-r")
     // https://mvnrepository.com/artifact/org.unbescape/unbescape
     implementation("org.unbescape:unbescape:1.1.6.RELEASE")
     // https://mvnrepository.com/artifact/com.apptasticsoftware/rssreader
-    implementation("com.apptasticsoftware:rssreader:3.5.0")
-
-    // https://mvnrepository.com/artifact/com.rometools/rome
-    // implementation("com.rometools:rome:2.1.0")
-    // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
-    // implementation("com.mysql:mysql-connector-j:8.2.0")
-    // https://mvnrepository.com/artifact/org.bitbucket.ijabz/jaudiotagger
-    // implementation("org.bitbucket.ijabz:jaudiotagger:v2.2.5")
-    // https://mvnrepository.com/artifact/de.androidpit/color-thief
-    // implementation("de.androidpit:color-thief:1.1.2")
-
-    // testImplementation(kotlin("test"))
+    implementation("com.apptasticsoftware:rssreader:3.6.0")
 }
 
 kotlin {
